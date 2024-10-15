b4eb2cd1886d90c4247cc42b49c00b6f68cb10ee
Nils van der Werf <n.vanderw.92@gmail.com>
2024-10-15 21:24:06 +1100

    Attempt to retrieve CRSF token

--- app/controllers/api/application_controller.rb
+++ app/controllers/api/application_controller.rb
@@ -1,3 +1,27 @@
-class ApplicationController < ActionController::Base
-  include DeviseTokenAuth::Concerns::SetUserByToken
+module Api
+  class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+
+    before_action :set_csrf_cookie
+
+    def csrf_token
+      render json: { csrf_token: form_authenticity_token }
+    end
+
+    private
+
+    def set_csrf_cookie
+      token = form_authenticity_token
+      cookies['CSRF-TOKEN'] = token if protect_against_forgery?
+      Rails.logger.debug "CSRF Token Set: #{token}"
+    end
+
+    protected
+
+    def verified_request?
+      token = request.headers['X-CSRF-Token']
+      Rails.logger.debug "CSRF Token Received: #{token}"
+      super || valid_authenticity_token?(session, token)
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,15 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   namespace :api do
+    get 'csrf_token', to: 'application#csrf_token'
     # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
     # Can be used by load balancers and uptime monitors to verify that the app is live.
-    mount_devise_token_auth_for 'User', at: 'auth'
+    # mount_devise_token_auth_for 'User', at: 'auth'
     get "up" => "rails/health#show", as: :rails_health_check
 
     # Defines the root path route ("/")
     # root "posts#index"
+    resources :users, only: [:create]
     resources :clients
     resources :appointments
     resources :visit_reports
